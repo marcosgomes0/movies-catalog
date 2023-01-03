a62665c5183994f963479b4fe878351a2e9bf2ef
Marcos Gomes <marcosmuriloribeirogomes@gmail.com>
2023-01-02 23:04:28 -0300

    dist

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/static/Header";
 import Footer from "./components/static/Footer";
+import Home from "./components/pages/Home";
+import Movie from "./components/pages/Movie";
+import Search from "./components/pages/Search";
+import Error from "./components/static/Error";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="grid">
-      <Header />
-      <Outlet />
-      <Footer/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<Movie />} />
+          <Route path="/search/:search" element={<Search />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Home from "./components/pages/Home";
-import Movie from "./components/pages/Movie";
-import Search from "./components/pages/Search";
-import Error from "./components/static/Error";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<Movie />} />
-          <Route path="/search/:search" element={<Search />} />
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
