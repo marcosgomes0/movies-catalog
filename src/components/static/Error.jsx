c7c81bc45c3775f950cbbec2841d73f404be067c
Marcos Gomes <marcosmuriloribeirogomes@gmail.com>
2023-01-02 22:43:15 -0300

    titulo error  404

--- src/components/static/Error.jsx
+++ src/components/static/Error.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 function Error() {
+
+document.title = "Error 404"
+
   return (
     <h2 className='text-center text-yellow-400 text-5xl my-24 uppercase'>Error 404</h2>
   )
